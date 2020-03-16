992e3d2a8182f25406c7467ed517f4c6bbeec751
Taras Vlasov <tarasvlasov@gmail.com>
2020-03-15 21:27:33 -0400

    initial commit

---
+++ src/components/CovidMap.js
@@ -0,0 +1,56 @@
+import React, { useState } from 'react'
+import './CovidMap.css'
+import useInterval from '../utils/useInterval'
+import countries from '../utils/countries.json'
+import { Map, TileLayer, Marker, Tooltip } from "react-leaflet";
+
+
+
+function CovidMap(){
+
+    const [covidData, setCovidData] = useState([]);
+    const [markers, setMArkers] = useState([]);
+    let cases = [];
+
+    useInterval(() => {
+      cases = [];
+
+      fetch('https://corona.lmao.ninja/countries')
+      .then(response => response.json())
+      .then(data => setCovidData(data));
+
+      covidData.forEach(cd => {
+          let country = countries.find(country => country.name.common === cd.country);
+          if(country){
+              cases.push(
+                {
+                  key: cd.country,
+                  cases: cd.cases,
+                  today: cd.todayCases,
+                  position: country.latlng
+                }
+              );
+          }
+      }
+      );
+
+      setMArkers(cases.map(covidcase => (
+        <Marker position={covidcase.position} opacity={0} >
+          <Tooltip direction="center" className="country-label" permanent><b>{covidcase.cases + '/' + covidcase.today}</b></Tooltip>
+        </Marker>
+      )))
+
+    }, 5000);
+
+    return (
+        <Map center={[51.505, -0.09]} zoom={3}>
+        <TileLayer
+          attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <div>{markers}</div>
+      </Map>
+    );
+}
+
+export default CovidMap;
