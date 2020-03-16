a774f0b06d62867bfda6b46fdc6227f0369faf22
Taras Vlasov <tarasvlasov@gmail.com>
2020-03-16 00:18:49 -0400

    favicon changed

--- src/components/CovidMap.js
+++ src/components/CovidMap.js
@@ -30,8 +30,8 @@ function CovidMap(){
                   position: country.latlng
                 }
               );
+            }
           }
-      }
       );
 
       setMArkers(cases.map(covidcase => (
